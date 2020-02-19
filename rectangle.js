af672b5d2d9295e8c2f06d2b1c43605201c64c55
sihan9 <1060192071@qq.com>
2020-02-19 10:39:13 +0800

    add 计算V01

--- rectangle.js
+++ rectangle.js
@@ -5,6 +5,17 @@ $(function add(x,y){
       $btn    = $('#calculate'),
       $per = $('#perimeter'),
       $area = $('#area');
+  /**
+  * decimalSave
+  * 小数点后面保留第n位
+  *
+  * @param num
+  * @param n
+  * @returns {undefined}
+  */
+  function decimalSave(num,n){
+    return Math.round(num * Math.pow(10,n)) / Math.pow(10,n);
+  }
   /*calc button click event*/
   $btn.click(function (){
     //get value
@@ -13,8 +24,14 @@ $(function add(x,y){
     //calculate
     var p = 2*(w+h);
     var a = w*h;
+    //decimalSave
+    //console.log(decimalSave(p,2));
+    p = decimalSave(p,2);
+    a = decimalSave(a,2);
     //output
     $per.val(p);
     $area.val(a);
   }) 
-})
+});
+
+
